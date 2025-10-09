e52cc2a4a50b31df7cfc424aab61a17b259c24fc
Nuri Khayal <Nuri.Khayal@bankofengland.co.uk>
2025-10-09 08:44:25 +0100

    modularise notebook

--- functions/process.py
+++ functions/process.py
@@ -60,24 +60,3 @@ def load_and_process_vacancy_data(number_of_previous_files):
         df = pd.concat([df, df_temp], ignore_index=True)
 
     return df
-
-def calculate_revision(row):
-    """
-    Calculate the magnitude of revisions across months.
-
-    The function computes the difference between the most recent valid value 
-    in the row and the first valid value. If there are no valid values 
-    in the row, it returns NaN.
-
-    Parameters:
-    row (pd.Series): A pandas Series representing a row of data.
-
-    Returns:
-    float or np.nan: The difference between the last valid value and 
-    the first valid value, or NaN if no valid values exist.
-    """
-    first_valid_index = row.first_valid_index()
-    if first_valid_index is not None:
-        return row.iloc[-1] - row[first_valid_index]
-    else:
-        return np.nan
